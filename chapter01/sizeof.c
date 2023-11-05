9c9ffbfaa11bffc8768bf8720d9397dd4055359c
kwonkidae <kkdosk@naver.com>
2023-11-05 11:18:08 +0900

    sizeof parentheses comment

--- chapter01/sizeof.c
+++ chapter01/sizeof.c
@@ -4,6 +4,7 @@ int main(void)
 {
   char c;
   // c99 에서 size_t 는 printf 포맷이 %zu 로 지정되어있다
+  // For the types, you do need the parentheses
   printf("%zu %zu\n", sizeof(char), sizeof c);
   int i;
   printf("%zu %zu\n", sizeof(int), sizeof i);
