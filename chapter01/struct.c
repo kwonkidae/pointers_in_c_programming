c7114d818d099071cfd001164dcb11806319fe66
kwonkidae <kkdosk@naver.com>
2023-11-07 08:04:35 +0900

    add struct align

--- chapter01/struct.c
+++ chapter01/struct.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#include <stdalign.h>
 struct data {
   int i;
-  double d;
   char c;
+  double d;
 };
 
 int main(void)
@@ -11,6 +11,7 @@ int main(void)
   struct data data;
   printf("sizeof data == %zu\n", sizeof data);
   printf("size of components == %zu\n", sizeof data.c + sizeof data.i + sizeof data.d);
+  printf("alignof(struct data) == %zu\n", alignof(struct data));
   printf("data   at %p\n", (void *)&data);
   printf("data.c at %p\n", (void *)&data.c);
   printf("data.i at %p\n", (void *)&data.i);
